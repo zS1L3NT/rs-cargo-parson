c434f9fa76abfbe0edfbecab5e039fbec8d82e32
Zechariah <zechariahtan144@gmail.com>
2022-02-19 03:07:40 +0800

    Removed unnecessary methods

--- src/json/array.rs
+++ src/json/array.rs
@@ -1,4 +1,4 @@
-use super::{null::JSONNull, object::JSONObject, value::JSONValue};
+use super::value::JSONValue;
 
 #[derive(Debug, Clone)]
 pub struct JSONArray {
@@ -30,79 +30,7 @@ impl JSONArray {
         self.data.push(value);
     }
 
-    pub fn get_string(&self, index: usize) -> Option<String> {
-        if self.data.get(index).is_none() {
-            return None;
-        }
-
-        if !self.data[index].is_string() {
-            panic!("Value at index is not of type String: {}", index);
-        }
-
-        Some(self.data[index].get_string())
-    }
-
-    pub fn get_number(&self, index: usize) -> Option<f64> {
-        if self.data.get(index).is_none() {
-            return None;
-        }
-
-        if !self.data[index].is_number() {
-            panic!("Value at index is not of type Number: {}", index);
-        }
-
-        Some(self.data[index].get_number())
-    }
-
-    pub fn get_boolean(&self, index: usize) -> Option<bool> {
-        if self.data.get(index).is_none() {
-            return None;
-        }
-
-        if !self.data[index].is_boolean() {
-            panic!("Value at index is not of type Boolean: {}", index);
-        }
-
-        Some(self.data[index].get_boolean())
-    }
-
-    pub fn get_null(&self, index: usize) -> Option<&JSONNull> {
-        if self.data.get(index).is_none() {
-            return None;
-        }
-
-        if !self.data[index].is_null() {
-            panic!("Value at index is not of type Null: {}", index);
-        }
-
-        Some(self.data[index].get_null())
-    }
-
-    pub fn get_array(&self, index: usize) -> Option<&JSONArray> {
-        if self.data.get(index).is_none() {
-            return None;
-        }
-
-        if !self.data[index].is_array() {
-            panic!("Value at index is not of type Array: {}", index);
-        }
-
-        Some(self.data[index].get_array())
-    }
-
-    pub fn get_object(&self, index: usize) -> Option<&JSONObject> {
-        if self.data.get(index).is_none() {
-            return None;
-        }
-
-        if !self.data[index].is_object() {
-            panic!("Value at index is not of type Object: {}", index);
-        }
-
-        Some(self.data[index].get_object())
-    }
-
-    pub fn get_value(&self, index: usize) -> Option<&JSONValue> {
+    pub fn get(&self, index: usize) -> Option<&JSONValue> {
         self.data.get(index)
     }
 }
--- src/json/object.rs
+++ src/json/object.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use super::{array::JSONArray, null::JSONNull, value::JSONValue};
+use super::value::JSONValue;
 
 #[derive(Debug, Clone)]
 pub struct JSONObject {
@@ -30,79 +30,11 @@ impl JSONObject {
         result
     }
 
+    pub fn get(&self, key: &str) -> Option<&JSONValue> {
+        self.data.get(key)
+    }
+
     pub fn set(&mut self, key: String, value: JSONValue) {
         self.data.insert(key, value);
-    }
-
-    pub fn get_string(&self, key: &str) -> Option<String> {
-        if self.data.get(key).is_none() {
-            return None;
-        }
-
-        if !self.data[key].is_string() {
-            panic!("Value for key is not of type String: {}", key);
-        }
-
-        Some(self.data[key].get_string())
-    }
-
-    pub fn get_number(&self, key: &str) -> Option<f64> {
-        if self.data.get(key).is_none() {
-            return None;
-        }
-
-        if !self.data[key].is_number() {
-            panic!("Value for key is not of type Number: {}", key);
-        }
-
-        Some(self.data[key].get_number())
-    }
-
-    pub fn get_boolean(&self, key: &str) -> Option<bool> {
-        if self.data.get(key).is_none() {
-            return None;
-        }
-
-        if !self.data[key].is_boolean() {
-            panic!("Value for key is not of type Boolean: {}", key);
-        }
-
-        Some(self.data[key].get_boolean())
-    }
-
-    pub fn get_null(&self, key: &str) -> Option<&JSONNull> {
-        if self.data.get(key).is_none() {
-            return None;
-        }
-
-        if !self.data[key].is_null() {
-            panic!("Value for key is not of type Null: {}", key);
-        }
-
-        Some(self.data[key].get_null())
-    }
-
-    pub fn get_array(&self, key: &str) -> Option<&JSONArray> {
-        if self.data.get(key).is_none() {
-            return None;
-        }
-
-        if !self.data[key].is_array() {
-            panic!("Value for key is not of type Array: {}", key);
-        }
-
-        Some(self.data[key].get_array())
-    }
-
-    pub fn get_object(&self, key: &str) -> Option<&JSONObject> {
-        if self.data.get(key).is_none() {
-            return None;
-        }
-
-        if !self.data[key].is_object() {
-            panic!("Value for key is not of type Object: {}", key);
-        }
-
-        Some(self.data[key].get_object())
     }
 }
